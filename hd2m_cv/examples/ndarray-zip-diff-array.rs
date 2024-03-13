28822a158a46d8b5937627fd6babadcf6427c510
preco21 <plusb21@gmail.com>
2024-03-14 01:45:06 +0900

    feat: add benchmark

--- hd2m_cv/examples/ndarray-zip-diff-array.rs
+++ hd2m_cv/examples/ndarray-zip-diff-array.rs
@@ -1,18 +1,26 @@
-use ndarray::Array3;
+use ndarray::Array2;
 use ndarray::Zip;
 
 fn main() {
-    type Array3f64 = Array3<f64>;
+    type Array2f64 = Array2<f64>;
 
-    const N: usize = 3;
-    let a = Array3f64::from_elem((N, N, N), 1.);
-    let b = Array3f64::from_elem(a.dim(), 2.);
-    let mut c = Array3::<i32>::zeros(a.dim());
+    const N: usize = 2000;
+    let a = Array2f64::from_elem((N, N), 1.);
+    let b = Array2f64::from_elem(a.dim(), 2.);
+    let mut c = Array2::<i32>::zeros(a.dim());
 
+    let start = std::time::Instant::now();
     // FINDINGS: Can use different types for the result array
     Zip::from(&mut c).and(&a).and(&b).par_for_each(|c, &a, &b| {
         *c += (a - b) as i32;
     });
+    println!("Elapsed (parallel): {:?}", start.elapsed());
+
+    let start = std::time::Instant::now();
+    Zip::from(&mut c).and(&a).and(&b).for_each(|c, &a, &b| {
+        *c += (a - b) as i32;
+    });
+    println!("Elapsed (normal): {:?}", start.elapsed());
 
     println!("{:?}", c);
 }
