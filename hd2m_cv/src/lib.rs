af1bd2a37ea3fcc0d7a4ce11d7707f70b3d5afe4
preco21 <plusb21@gmail.com>
2024-03-16 01:35:13 +0900

    feat: add subset of cv_convert module

--- hd2m_cv/src/lib.rs
+++ hd2m_cv/src/lib.rs
@@ -1,14 +1,3 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod cv_convert;
+pub mod finder;
+pub mod screen;
