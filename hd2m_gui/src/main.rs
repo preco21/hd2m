33d8fb88df6d5330f78e22792693a3f9e375c81d
preco21 <plusb21@gmail.com>
2024-03-18 03:38:54 +0900

    fix: implement capture every 2s

--- hd2m_gui/src/main.rs
+++ hd2m_gui/src/main.rs
@@ -14,7 +14,7 @@ fn main() -> iced::Result {
     std::thread::spawn(move || {
         for i in 0.. {
             sender.send(i).unwrap();
-            std::thread::sleep(std::time::Duration::from_millis(200));
+            std::thread::sleep(std::time::Duration::from_millis(2000));
         }
     });
 
@@ -27,6 +27,7 @@ struct UiFlags {
 
 struct Ui {
     receiver: RefCell<Option<mpsc::UnboundedReceiver<i32>>>,
+    tick_tx: mpsc::Sender<()>,
     capture_trigger: (
         mpsc::Sender<oneshot::Sender<cv::core::Mat>>,
         mpsc::Receiver<oneshot::Sender<cv::core::Mat>>,
@@ -36,6 +37,7 @@ struct Ui {
 
 #[derive(Debug, Clone)]
 enum Message {
+    None,
     ExternalMessageReceived(i32),
 }
 
@@ -46,12 +48,42 @@ impl Application for Ui {
     type Flags = UiFlags;
 
     fn new(flags: UiFlags) -> (Self, Command<Message>) {
+        let (tick_tx, mut tick_rx) = mpsc::channel(1);
         let app = Ui {
             receiver: RefCell::new(Some(flags.receiver)),
+            tick_tx,
             capture_trigger: mpsc::channel(1),
             num: 0,
         };
-        (app, Command::none())
+        (
+            app,
+            Command::perform(
+                async move {
+                    let (capture_chan_tx, capture_chan_rx) = mpsc::channel(1);
+
+                    let mgr = CaptureManager::new(CaptureManagerConfig {
+                        window_title: "Code".to_owned(),
+                    })
+                    .unwrap();
+
+                    tokio::spawn(async move {
+                        mgr.start(capture_chan_rx).await.unwrap();
+                    });
+
+                    tokio::spawn(async move {
+                        loop {
+                            tick_rx.recv().await;
+                            let (cap_tx, cap_rx) = oneshot::channel();
+                            let _ = capture_chan_tx.send(cap_tx).await;
+                            let result = cap_rx.await.unwrap();
+                            let cv: RgbaImage = result.try_into_cv().unwrap();
+                            cv.save("screenshot.png").unwrap();
+                        }
+                    });
+                },
+                |_| Message::None,
+            ),
+        )
     }
 
     fn title(&self) -> String {
@@ -63,44 +95,45 @@ impl Application for Ui {
             Message::ExternalMessageReceived(num) => {
                 self.num = num;
             }
+            Message::None => {}
         }
         Command::none()
     }
 
     fn subscription(&self) -> Subscription<Message> {
-        // iced::subscription::unfold(
-        //     "led changes",
-        //     (self.receiver.take(), tick_tx),
-        //     move |(mut receiver, tick_tx)| async move {
-        //         let num = receiver.as_mut().unwrap().recv().await.unwrap();
-        //         tick_tx.send(());
-        //         (Message::ExternalMessageReceived(num), (receiver, tick_tx))
-        //     },
-        // );
+        iced::subscription::unfold(
+            "led changes",
+            (self.receiver.take(), self.tick_tx.clone()),
+            move |(mut receiver, tick_tx)| async move {
+                let num = receiver.as_mut().unwrap().recv().await.unwrap();
+                tick_tx.send(()).await.unwrap();
+                (Message::ExternalMessageReceived(num), (receiver, tick_tx))
+            },
+        )
 
-        iced::subscription::unfold("screen capture", 100, move |mut output| async move {
-            let (tick_tx, tick_rx) = mpsc::channel(1);
+        // iced::subscription::unfold("screen capture", 100, move |mut output| async move {
+        //     let (tick_tx, tick_rx) = mpsc::channel(1);
 
-            let (capture_chan_tx, capture_chan_rx) = mpsc::channel(1);
+        //     let (capture_chan_tx, capture_chan_rx) = mpsc::channel(1);
 
-            let mgr = CaptureManager::new(CaptureManagerConfig {
-                window_title: "Code".to_owned(),
-            })
-            .unwrap();
+        //     let mgr = CaptureManager::new(CaptureManagerConfig {
+        //         window_title: "Code".to_owned(),
+        //     })
+        //     .unwrap();
 
-            tokio::spawn(async move {
-                mgr.start(capture_chan_rx).await.unwrap();
-            });
+        //     tokio::spawn(async move {
+        //         mgr.start(capture_chan_rx).await.unwrap();
+        //     });
 
-            loop {
-                tick_rx.recv().await;
-                let (cap_tx, cap_rx) = oneshot::channel();
-                let _ = capture_chan_tx.send(cap_tx).await;
-                let result = cap_rx.await.unwrap();
-                let cv: RgbaImage = result.try_into_cv().unwrap();
-                cv.save("screenshot.png").unwrap();
-            }
-        })
+        //     loop {
+        //         tick_rx.recv().await;
+        //         let (cap_tx, cap_rx) = oneshot::channel();
+        //         let _ = capture_chan_tx.send(cap_tx).await;
+        //         let result = cap_rx.await.unwrap();
+        //         let cv: RgbaImage = result.try_into_cv().unwrap();
+        //         cv.save("screenshot.png").unwrap();
+        //     }
+        // })
     }
 
     fn view(&self) -> Element<Message> {
