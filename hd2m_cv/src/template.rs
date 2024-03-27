3d2aab863f6c16f252b5eab5888efc2c34f110f1
Preco Kim <plusb21@gmail.com>
2024-03-27 15:10:07 +0900

    docs: add some links

--- hd2m_cv/src/template.rs
+++ hd2m_cv/src/template.rs
@@ -29,6 +29,7 @@ pub fn resize_template_scale(template: &cv::core::Mat, scale: f64) -> Result<cv:
         scale,
         scale,
         // This will produce the near-same result in terms of pixel patterns as the original template image provided
+        // For details: https://stackoverflow.com/questions/5358700/template-match-different-sizes-of-template-and-image
         // See also: https://docs.opencv.org/3.4/da/d54/group__imgproc__transform.html#gga5bb5a1fea74ea38e1a5445ca803ff121aa5521d8e080972c762467c45f3b70e6c
         cv::imgproc::INTER_NEAREST_EXACT,
     )?;
