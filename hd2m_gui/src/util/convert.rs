d7ed15f142d6e23f1b6d05496f236bc8a8898316
preco21 <plusb21@gmail.com>
2024-06-29 03:07:06 +0900

    fix: fix error

--- hd2m_gui/src/util/convert.rs
+++ hd2m_gui/src/util/convert.rs
@@ -5,7 +5,7 @@ use std::os::raw::c_void;
 pub fn convert_frame_to_mat(from: &mut windows_capture::frame::Frame) -> Result<cv::core::Mat> {
     let mut data = from.buffer()?;
     let mat = unsafe {
-        cv::core::Mat::new_rows_cols_with_data(
+        cv::core::Mat::new_rows_cols_with_data_unsafe(
             data.height() as i32,
             data.width() as i32,
             cv::core::CV_8UC4,
