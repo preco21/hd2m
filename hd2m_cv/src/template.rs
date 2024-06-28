d8f79a16eb967530c444d9ce3777d9a4c694bacd
preco21 <plusb21@gmail.com>
2024-06-29 02:22:23 +0900

    feat: implement template matcher

--- hd2m_cv/src/template.rs
+++ hd2m_cv/src/template.rs
@@ -1,38 +1,65 @@
 use anyhow::Result;
-use cv::core::MatTrait;
 use opencv::{self as cv};
 
-pub fn match_template_with_mask(
-    source: &cv::core::Mat,
-    template: &cv::core::Mat,
-    mask: Option<&cv::core::Mat>,
-) -> Result<cv::core::Mat> {
-    let mask = mask.unwrap_or(template);
-
-    let mut res = cv::core::Mat::default();
-    cv::imgproc::match_template(
-        source,
-        template,
-        &mut res,
-        cv::imgproc::TM_CCORR_NORMED,
-        mask,
-    )?;
-
-    Ok(res)
+pub struct TemplateMatcher {
+    pub template_mat: cv::core::Mat,
 }
 
-pub fn resize_template_scale(template: &cv::core::Mat, scale: f64) -> Result<cv::core::Mat> {
-    let mut res = cv::core::Mat::default();
-    cv::imgproc::resize(
-        template,
-        &mut res,
-        Default::default(),
-        scale,
-        scale,
-        // This will produce the near-same result in terms of pixel patterns as the original template image provided
-        // For details: https://stackoverflow.com/questions/5358700/template-match-different-sizes-of-template-and-image
-        // See also: https://docs.opencv.org/3.4/da/d54/group__imgproc__transform.html#gga5bb5a1fea74ea38e1a5445ca803ff121aa5521d8e080972c762467c45f3b70e6c
-        cv::imgproc::INTER_NEAREST_EXACT,
-    )?;
-    Ok(res)
+impl TemplateMatcher {
+    pub fn new(template: &cv::core::Mat) -> Result<Self> {
+        Ok(Self {
+            template_mat: canny(template)?,
+        })
+    }
+
+    pub fn match_template(&self, source: &cv::core::Mat) -> Result<cv::core::Mat> {
+        let edged = canny(source)?;
+        let mut res = cv::core::Mat::default();
+        cv::imgproc::match_template(
+            &edged,
+            &self.template_mat,
+            &mut res,
+            cv::imgproc::TM_CCOEFF_NORMED,
+            // Use the template as the mask too
+            &self.template_mat,
+        )?;
+        Ok(res)
+    }
+
+    pub fn resize_template(&mut self, width: i32, height: i32) -> Result<cv::core::Mat> {
+        let mut res = cv::core::Mat::default();
+        cv::imgproc::resize(
+            &self.template_mat,
+            &mut res,
+            cv::core::Size::new(width, height),
+            0.0,
+            0.0,
+            cv::imgproc::INTER_NEAREST,
+        )?;
+        self.template_mat = res;
+        Ok(res)
+    }
+
+    pub fn resize_template_scale(&mut self, scale: f64) -> Result<()> {
+        let mut res = cv::core::Mat::default();
+        cv::imgproc::resize(
+            &self.template_mat,
+            &mut res,
+            Default::default(),
+            scale,
+            scale,
+            // This will produce the near-accurate result in terms of pixel patterns as the original template image provided
+            // For details: https://stackoverflow.com/questions/5358700/template-match-different-sizes-of-template-and-image
+            // See also: https://docs.opencv.org/3.4/da/d54/group__imgproc__transform.html#gga5bb5a1fea74ea38e1a5445ca803ff121aa5521d8e080972c762467c45f3b70e6c
+            cv::imgproc::INTER_NEAREST_EXACT,
+        )?;
+        self.template_mat = res;
+        Ok(())
+    }
+}
+
+fn canny(template: &cv::core::Mat) -> Result<cv::core::Mat> {
+    let mut canny_source = cv::core::Mat::default();
+    cv::imgproc::canny(&template, &mut canny_source, 150.0, 300.0, 3, true)?;
+    Ok(canny_source)
 }
