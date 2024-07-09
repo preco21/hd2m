5af2398701fd26d35a58d8aaf23210c3a4fef987
preco21 <plusb21@gmail.com>
2024-07-10 02:38:06 +0900

    feat: add poc for template matcher sequencing

--- hd2m_cv/examples/template-matcher-sequencing.rs
+++ hd2m_cv/examples/template-matcher-sequencing.rs
@@ -3,6 +3,12 @@ use hd2m_cv::{Direction, TryFromCv, TryIntoCv};
 use image::{DynamicImage, GrayImage, RgbaImage};
 use opencv as cv;
 
+const MARGIN_Y_ANCHOR: i32 = 22 + 10;
+const ANCHOR_BOX: i32 = 25;
+
+const BOX_SIZE: i32 = 70;
+const COEFF: f32 = 0.0152777777777778;
+
 fn main() -> Result<()> {
     let start = std::time::Instant::now();
 
@@ -14,57 +20,42 @@ fn main() -> Result<()> {
     let anchor_mch = hd2m_cv::TemplateMatcher::new(&anchor_img.try_into_cv()?)?;
     let anchor_res = anchor_mch.match_template(&source.try_into_cv()?)?;
 
-    let mut min_val = 0.0f64;
-    let mut max_val = 0.0f64;
-    let mut min_loc = cv::core::Point::default();
-    let mut max_loc = cv::core::Point::default();
+    let pos = anchor_res.position()?;
 
-    cv::core::min_max_loc(
-        &anchor_res,
-        Some(&mut min_val),
-        Some(&mut max_val),
-        Some(&mut min_loc),
-        Some(&mut max_loc),
-        &cv::core::no_array(),
-    )?;
-
-    println!(
-        "vals: {:?} {:?} {:?} {:?}",
-        min_val, max_val, min_loc, max_loc
-    );
-
-    // let img_tmp = image::open("./examples/temp.png")?.to_rgba8();
-    // let matcher = hd2m_cv::TemplateMatcher::new(&img_tmp.try_into_cv()?)?;
-
-    // let mut min_val = 0.0f64;
-    // let mut max_val = 0.0f64;
-    // let mut min_loc = cv::core::Point::default();
-    // let mut max_loc = cv::core::Point::default();
-
-    // println!("Elapsed: {:?}", start.elapsed());
-    // cv::core::min_max_loc(
-    //     &res,
-    //     Some(&mut min_val),
-    //     Some(&mut max_val),
-    //     Some(&mut min_loc),
-    //     Some(&mut max_loc),
-    //     &cv::core::no_array(),
-    // )?;
-
-    // println!(
-    //     "vals: {:?} {:?} {:?} {:?}",
-    //     min_val, max_val, min_loc, max_loc
-    // );
+    // Reposition the anchor to the stratgem box
+    let pos = cv::core::Point::new(pos.x, pos.y + ANCHOR_BOX + MARGIN_Y_ANCHOR);
 
     let mut dst_img = dst_img.try_into_cv()?;
-    cv::imgproc::rectangle(
-        &mut dst_img,
-        cv::core::Rect::from_points(min_loc, max_loc),
-        cv::core::VecN([255., 255., 0., 0.]),
-        2,
-        cv::imgproc::LINE_8,
-        0,
-    )?;
+
+    println!("pos: {:?}", pos);
+
+    // cv::imgproc::rectangle(
+    //     &mut dst_img,
+    //     cv::core::Rect::from_points(pos, cv::core::Point::new(pos.x + 300, pos.y + 500)),
+    //     cv::core::VecN([255., 255., 0., 0.]),
+    //     2,
+    //     cv::imgproc::LINE_8,
+    //     0,
+    // )?;
+
+    let x = pos.x + 10;
+    for i in 0..10 {
+        let y = pos.y + i * BOX_SIZE;
+        println!("x: {}", x);
+        println!("y: {}", y);
+
+        cv::imgproc::rectangle(
+            &mut dst_img,
+            cv::core::Rect::from_points(
+                cv::core::Point::new(x, y),
+                cv::core::Point::new(x + 300, y + BOX_SIZE),
+            ),
+            cv::core::VecN([255., 255., 255., 0.]),
+            1,
+            cv::imgproc::LINE_8,
+            0,
+        )?;
+    }
 
     GrayImage::try_from_cv(&dst_img)?.save("./result.png")?;
 
